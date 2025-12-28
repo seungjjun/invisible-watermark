5683424a2388e41f91ec56618934a422baa071e4
seungjjun <stw550@gmail.com>
2025-12-28 16:05:12 +0900

    feat: add watermark-api module

---
+++ watermark-api/src/main/java/com/seungjjun/watermark/api/WatermarkApiApplication.java
@@ -0,0 +1,12 @@
+package com.seungjjun.watermark.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermarkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatermarkApiApplication.class, args);
+    }
+}
